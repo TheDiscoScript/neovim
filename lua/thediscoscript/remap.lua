3a1dd1a8e1851190408a1ba2d480c3eefa3c1bc6
Daniel Wollmann <wollmann@iot-water.cz>
2023-09-11 19:11:14 +0200

    Added commit - trying commit with neovim

--- lua/thediscoscript/remap.lua
+++ lua/thediscoscript/remap.lua
@@ -45,6 +45,8 @@ vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 -- replace words i am on
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+
+-- chmod +x
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 vim.keymap.set("n", "<leader><leader>", function()
